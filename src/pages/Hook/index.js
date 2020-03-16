40bdb81c3b5c81338bd10bd7cc108319029cc522
lamphc <lamphc@live.com>
2020-03-16 22:50:50 +0800

    feat:add css module

--- src/pages/Hook/index.js
+++ src/pages/Hook/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
+import sty from './index.module.css'
+
 function Example() {
   // 声明一个新的叫做 “count” 的 state 变量
   const [count, setCount] = useState(0);
@@ -10,11 +12,11 @@ function Example() {
   });
 
   return (
-    <div>
-      <p>You clicked {count}times</p>
-      <button onClick={() => setCount(count + 1)}>
+    <div className={sty.box}>
+      <p>You clicked {count} times</p>
+      <a className={[sty.btn, sty.blu].join(' ')} onClick={() => setCount(count + 1)}>
         Click me
-      </button>
+      </a>
     </div>
   );
 }
